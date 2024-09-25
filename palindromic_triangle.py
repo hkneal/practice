914a5d2fe75d54ea573b2469e2ac24eb252c11f8
Hiram Neal <hiramk@comcast.net>
2024-09-24 19:40:20 -0700

    Adding print practice

---
+++ palindromic_triangle.py
@@ -0,0 +1,16 @@
+"""
+You are given a positive integer N.
+Your task is to print a palindromic triangle of size N.
+
+For example, a palindromic triangle of size 5 is:
+
+1
+121
+12321
+1234321
+123454321
+"""
+x = 5
+for i in range(1, x+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    # print(''.join([str(x) for x in range(1, i+1)]),''.join([str(x) for x in range(i-1, 0, -1)]), sep="")
+    print(sum(list(map(lambda x: 1*10**x, range(i))))**2)
