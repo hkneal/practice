b91247cd227ee8b8bec96126c8ff1cc7076b1146
Hiram Neal <hiramk@comcast.net>
2024-11-05 20:12:36 -0800

    fibonacci practice

--- fibonacci.py
+++ fibonacci.py
@@ -8,10 +8,12 @@ def fib(n: int) -> int:
 
     return fib(n-1) + fib(n-2)
 
+num = 42
 timestamp = datetime.now()
-print(fib(40))
+fib(num)
 time_spent = datetime.now() - timestamp
-print(f"{time_spent.min} minutes, {time_spent.seconds} seconds, {time_spent.microseconds} microseconds")
+print(f"Ran fib {num}")
+print(f"{time_spent.seconds} seconds, {time_spent.microseconds} microseconds")
 
 #0 1 2 3 4 5 6  7 8  9  10
 #0 1 1 2 3 5 8 13 21 34 55
@@ -24,7 +26,9 @@ def build_fibs(n:int) ->int:
         fibs.append(fibs[-1] + fibs[-2])
     return fibs[n]
 
+num = 1000
 timestamp = datetime.now()
-print(build_fibs(1000))
+build_fibs(num)
 time_spent = datetime.now() - timestamp
+print(f"Ran fib {num}")
 print(f"{time_spent.seconds} seconds, {time_spent.microseconds} microseconds")
